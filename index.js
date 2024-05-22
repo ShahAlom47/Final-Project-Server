a93d32dff4b8c82461ed589e8583f16fcad60ae5
sah alom <sahalom4729@gmail.com>
2024-05-22 21:44:44 +0400

    added users related Api

--- index.js
+++ index.js
@@ -5,17 +5,17 @@ const port = process.env.PORT || 3000;
 var cors = require('cors')
 
 
- 
+
 
 app.use(express.json())
 app.use(
-    cors({
-      origin: [
-        "http://localhost:5173",
-      ],
-      credentials: true,
-    })
-  );
+  cors({
+    origin: [
+      "http://localhost:5173",
+    ],
+    credentials: true,
+  })
+);
 
 
 
@@ -39,80 +39,128 @@ async function run() {
     // // Connect the client to the server	(optional starting in v4.7)
     // await client.connect();
 
-    const menuCollection =  client.db("final_p_DB").collection('menuData')
-    const cardCollection =  client.db("final_p_DB").collection('cardData')
-    const reviewsCollection =  client.db("final_p_DB").collection('reviewData')
+    const menuCollection = client.db("final_p_DB").collection('menuData')
+    const cardCollection = client.db("final_p_DB").collection('cardData')
+    const reviewsCollection = client.db("final_p_DB").collection('reviewData')
+    const userCollection = client.db("final_p_DB").collection('usersData')
 
-    app.get('/menu', async(req, res) => {
-        const category=req.query.category
-       
-        const query = {}
-        if (category) {
-            query.category = category;
-        }
-     
-        const result = await menuCollection.find(query).toArray();
-        res.send(result)
-      })
+    // user related  api
 
-    //  add  food card 
-      app.post('/foodCard',async (req,res)=>{
+    app.post('/addUser', async (req, res) => {
 
-        const {cardInfo}= req.body;
-        const result = await cardCollection.insertOne(cardInfo)
-        res.send(result)
-       
-      })
+      const userInfo = req.body
+      const query = { email: userInfo.email }
+      const existingUser = await userCollection.findOne()
+      if (existingUser) {
+        return res.send({ message: 'user already exist', insertedId: null })
+      }
 
-      // get food Card by email
-
-      app.get('/foodCard', async (req,res)=>{
-        const {email}=req.query;
-        const filter={userEmail:email}
-        const result = await cardCollection.find(filter).toArray()
-        res.send(result)
-      })
-
-
-      // get food Card
-
-      app.get('/cart', async(req, res) => {
-        const cursor = cardCollection.find();
-        const result =  await cursor.toArray()
-        res.send(result)
+      const result = await userCollection.insertOne(userInfo)
+      res.send(result)
     })
 
 
-      app.delete('/deleteCart/:id', async (req,res)=>{
-        const id=req.params.id
-        const query={cardId:id};
-        const result = await cardCollection.deleteOne(query)
-        res.send(result)
-        console.log(id);
-      })
+    app.get('/allUsers', async (req, res) => {
+      const result = await userCollection.find().toArray()
+      res.send(result)
+    })
    
+  //  delete user 
+    app.delete('/user/:id', async (req, res) => {
+      const id =req.params.id
+      console.log(id);
+      const query = {_id: new ObjectId(id)}
+      const result = await userCollection.deleteOne(query)
+      res.send(result)
+    })
 
-      // get singel cart
+// admin making
 
-      app.get('/menu/:id', async(req,res)=>{
-        const id=req.params.id;
-        const filter={_id: new ObjectId(id)}
-        const result = await menuCollection.findOne(filter)
-        res.send(result)
-        
- 
-      })
+app.patch('/user/admin/:id', async (req, res) => {
+  const id =req.params.id
+  console.log(id);
+  const filter = {_id: new ObjectId(id)}
+  const updatedDocs={
+      $set:{
+        role:'admin'
+      }
+  }
+  const result = await userCollection.updateOne(filter,updatedDocs)
+  res.send(result)
+})
+
+// menu related Api
+
+    app.get('/menu', async (req, res) => {
+      const category = req.query.category
+
+      const query = {}
+      if (category) {
+        query.category = category;
+      }
+
+      const result = await menuCollection.find(query).toArray();
+      res.send(result)
+    })
+
+    //  add  food card 
+    app.post('/foodCard', async (req, res) => {
+
+      const { cardInfo } = req.body;
+      const result = await cardCollection.insertOne(cardInfo)
+      res.send(result)
+
+    })
+
+    // get food Card by email
+
+    app.get('/foodCard', async (req, res) => {
+      const { email } = req.query;
+      const filter = { userEmail: email }
+      const result = await cardCollection.find(filter).toArray()
+      res.send(result)
+    })
 
 
-      // get review data 
+    // get food Card
 
-      app.get('/reviews',async(req,res)=>{
-        const cursor = reviewsCollection.find();
-        const result =  await cursor.toArray()
-        res.send(result)
+    app.get('/cart', async (req, res) => {
+      const cursor = cardCollection.find();
+      const result = await cursor.toArray()
+      res.send(result)
+    })
 
-      })
-   
+
+    app.delete('/deleteCart/:id', async (req, res) => {
+      const id = req.params.id
+      const query = { cardId: id };
+      const result = await cardCollection.deleteOne(query)
+      res.send(result)
+      console.log(id);
+    })
+
+
+    // get singel cart
+
+    app.get('/menu/:id', async (req, res) => {
+      const id = req.params.id;
+      const filter = { _id: new ObjectId(id) }
+      const result = await menuCollection.findOne(filter)
+      res.send(result)
+
+
+    })
+
+
+    // get review data 
+
+    app.get('/reviews', async (req, res) => {
+      const cursor = reviewsCollection.find();
+      const result = await cursor.toArray()
+      res.send(result)
+
+    })
+
 
 
 
